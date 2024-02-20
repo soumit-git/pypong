6677d0300c9027b98a24530f487e2415a2696ec0
Soumit Goswami <soumit.goswami12@gmail.com>
2024-02-20 14:00:37 +0530

    Add code for the paddles and the ball

--- pong.py
+++ pong.py
@@ -6,6 +6,32 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Paddle a
+paddle_a = Turtle()
+paddle_a.speed(0)
+paddle_a.shape('square')
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle b
+paddle_b = Turtle()
+paddle_b.speed(0)
+paddle_b.shape('square')
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = Turtle()
+ball.speed(0)
+ball.shape('circle')
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
 while True:
   wn.update()
   
