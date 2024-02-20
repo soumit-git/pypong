494a1a23c5528cec9e5a8dded5c7c198ffdf186a
Soumit Goswami <soumit.goswami12@gmail.com>
2024-02-20 23:48:33 +0530

    Add scoring system

--- pong.py
+++ pong.py
@@ -6,6 +6,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle a
 paddle_a = Turtle()
 paddle_a.speed(0)
@@ -33,6 +37,15 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.2
 ball.dy = -0.2
+
+# Pen
+pen = Turtle()
+pen.penup()
+pen.color('white')
+pen.speed(0)
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write('Player A: 0  Player B: 0', align='center', font=('Courier', 18, 'normal'))
 
 def paddle_a_up():
   y = paddle_a.ycor()
@@ -75,9 +88,15 @@ while True:
   if ball.xcor() > 390:
     ball.goto(0, 0)
     ball.dx *= -1
+    score_a += 1
+    pen.clear()
+    pen.write(f'Player A: {score_a}  Player B: {score_b}', align='center', font=('Courier', 18, 'normal'))
   if ball.xcor() < -390:
     ball.goto(0, 0)
     ball.dx *= -1
+    score_b += 1
+    pen.clear()
+    pen.write(f'Player A: {score_a}  Player B: {score_b}', align='center', font=('Courier', 18, 'normal'))
    
   if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
     ball.setx(340)
@@ -86,4 +105,3 @@ while True:
   if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
     ball.setx(-340)
     ball.dx *= -1
-  
